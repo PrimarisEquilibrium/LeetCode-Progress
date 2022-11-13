fbfbf70235e16dea822b2ee4ad0aaf445fda90c8
PrimarisEquilibrium <incorrectclock1@gmail.com>
2022-11-13 10:29:53 -0500

    first commit

---
+++ islandMaxArea.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+def maxAreaOfIsland(grid):
+    rows, cols = len(grid), len(grid[0])
+    visited = set()
+
+    def bfs(r, c): # BFS function to find the amount of tiles in one island
+        q = deque()
+        visited.add((r, c))
+        q.append((r, c))
+
+        counter = 1
+        while q:
+            row, col = q.popleft()
+            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+            for dr, dc in directions:
+                r, c = row + dr, col + dc
+                if (r<0 or r>=rows or c<0 or c>=cols or grid[r][c] == 0 or (r, c) in visited): continue
+                # Valid island tile
+                visited.add((r, c))
+                q.append((r, c))
+                counter += 1
+        return counter
+    
+    island_area = 0
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1 and (r, c) not in visited:
+                cur_island = bfs(r, c)
+                if cur_island > island_area:
+                    island_area = cur_island
+    return island_area
+
+grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+        [0,0,0,0,0,0,0,1,1,1,0,0,0],
+        [0,1,1,0,1,0,0,0,0,0,0,0,0],
+        [0,1,0,0,1,1,0,0,1,0,1,0,0],
+        [0,1,0,0,1,1,0,0,1,1,1,0,0],
+        [0,0,0,0,0,0,0,0,0,0,1,0,0],
+        [0,0,0,0,0,0,0,1,1,1,0,0,0],
+        [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+print(maxAreaOfIsland(grid))
